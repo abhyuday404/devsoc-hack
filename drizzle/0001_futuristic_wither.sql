5556c80c3041c5e17d48e8bc05d57148b1555c81
thinkter <ashmangamer0406@gmail.com>
2026-02-09 03:57:38 +0530

    feat: working auth

---
+++ drizzle/0001_futuristic_wither.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "user" ALTER COLUMN "email_verified" SET DATA TYPE boolean USING CASE WHEN "email_verified" IS NOT NULL THEN true ELSE false END;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "email_verified" SET DEFAULT false;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "email_verified" SET NOT NULL;
